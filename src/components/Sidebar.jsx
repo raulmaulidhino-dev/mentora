33590616ed753ff026c4020db5b4cfe130975032
Raul Maulidhino <raulahmadm314@gmail.com>
2024-12-15 06:54:30 +0700

    fix: add active navigation logic

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import Navigasi from "./Navigasi";
 
-const Sidebar = () => {
+const Sidebar = ({ active }) => {
     return (
         <aside className="bg-white flex flex-col p-2 h-screen w-1/4">
             <h2 className="text-center text-2xl font-extrabold my-4">MENTORA</h2>
-            <Navigasi />
+            <Navigasi active={active} />
             <button className="text-white font-bold bg-red-600 rounded-xl py-2 px-4 mx-auto">SIGN OUT</button>
         </aside>
     )
