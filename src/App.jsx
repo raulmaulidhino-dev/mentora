f79723a461e877be39097426af51ccee77741cb5
Raul Maulidhino <raulahmadm314@gmail.com>
2024-12-15 06:49:50 +0700

    feat: add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
 import Registrasi from './components/Registrasi'
 import Beranda from './components/Beranda'
+import TugasSaya from './components/TugasSaya'
+import TugasTerbaruSaya from './components/TugasTerbaruSaya'
+import ProfilSiswa from './components/ProfilSiswa'
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
       <Route path="/login" exact element={<Login />} />
       <Route path="/registrasi" element={<Registrasi />} />
       <Route path="/beranda" element={<Beranda />} />
+      <Route path="/mata-pelajaran" element={<TugasSaya />} />
+      <Route path="/tugas-saya" element={<TugasTerbaruSaya />} />
+      <Route path="/profil" element={<ProfilSiswa />} />
     </Routes>
   )
 }
