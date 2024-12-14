168d6f0aa75f05ca68ed3d0383751e13f2909c0f
Raul Maulidhino <raulahmadm314@gmail.com>
2024-12-14 09:19:30 +0700

    feat: add route /beranda

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
 import Registrasi from './components/Registrasi'
+import Beranda from './components/Beranda'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/login" exact element={<Login />} />
       <Route path="/registrasi" element={<Registrasi />} />
+      <Route path="/beranda" element={<Beranda />} />
     </Routes>
   )
 }
