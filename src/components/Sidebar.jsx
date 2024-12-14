fd3b60d1f49f0822f44d11b081b929f2a6ae18ec
Raul Maulidhino <raulahmadm314@gmail.com>
2024-12-14 09:18:27 +0700

    add Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import Navigasi from "./Navigasi";
+
+const Sidebar = () => {
+    return (
+        <aside className="bg-white flex flex-col p-2 h-screen w-1/4">
+            <h2 className="text-center text-2xl font-extrabold my-4">MENTORA</h2>
+            <Navigasi />
+            <button className="text-white font-bold bg-red-600 rounded-xl py-2 px-4 mx-auto">SIGN OUT</button>
+        </aside>
+    )
+}
+
+export default Sidebar;
