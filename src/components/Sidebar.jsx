fcb8f6851041145750c2fbb19e25333b540c328b
Raul Maulidhino <raulahmadm314@gmail.com>
2024-12-15 13:43:38 +0700

    fix: edit Sidebar layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,8 @@
-import Navigasi from "./Navigasi";
+import Navigasi from "./Navigasi.jsx";
 
 const Sidebar = ({ active }) => {
     return (
-        <aside className="bg-white flex flex-col p-2 h-screen w-1/4">
+        <aside className="bg-white flex flex-col p-2 h-screen basis-[25vw]">
             <h2 className="text-center text-2xl font-extrabold my-4">MENTORA</h2>
             <Navigasi active={active} />
             <button className="text-white font-bold bg-red-600 rounded-xl py-2 px-4 mx-auto">SIGN OUT</button>
