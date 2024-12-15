16e24eef6c7c072431d5f248f9cbb8ea2603d1f4
Raul Maulidhino <raulahmadm314@gmail.com>
2024-12-15 13:48:03 +0700

    fix: add object(s) as small database(s)

--- src/components/TugasTerbaruSaya.jsx
+++ src/components/TugasTerbaruSaya.jsx
@@ -1,18 +1,25 @@
-import Sidebar from './Sidebar'
-import TugasTerbaru from './TugasTerbaru'
+import Sidebar from './Sidebar.jsx'
+import TugasTerbaru from './TugasTerbaru.jsx'
 
 const TugasTerbaruSaya = () => {
     let activeNav = "2";
 
+    let tasks = [
+        {subject:"Kimia", title:"Proyek Akhir: Mengamati reaksi pembakaran alkohol", dateCreated:"Minggu, 24 November 2024"},
+        {subject:"Pendidikan Pancasila", title:"Saya ber-Pancasila", dateCreated:"Sabtu, 23 November 2024"},
+        {subject:"Matematika", title:"Penerapan Turunan dalam Kehidupan Sehari-hari", dateCreated:"Rabu, 20 November 2024"},
+        {subject:"Pendididkan Agama Islam", title:"Peradaban Islam dari Masa ke Masa", dateCreated:"Kamis, 14 November 2024"}
+    ];
+
     return (
         <div className="bg-gray-100 flex h-screen">
             <Sidebar active={ activeNav } />
-            <main className="flex-grow p-4">
+            <main className="p-4 basis-[75vw]">
                 <h1 className="text-3xl font-extrabold mb-[0.5em]">TUGAS TERBARU SAYA</h1>
                 <section className="grid gap-4">
-                    <TugasTerbaru index="01" subject="Biologi" title="Jelaskan proses pembelahan sel pada makhluk hidup bersel tunggal!" dateCreated="Rabu, 22 Desember, 16:00 WIB" />
-                    <TugasTerbaru index="02" subject="Biologi" title="Jelaskan proses pembelahan sel pada makhluk hidup bersel tunggal!" dateCreated="Rabu, 22 Desember, 16:00 WIB" />
-                    <TugasTerbaru index="03" subject="Biologi" title="Jelaskan proses pembelahan sel pada makhluk hidup bersel tunggal!" dateCreated="Rabu, 22 Desember, 16:00 WIB" />
+                    {tasks.map((task, index) => {
+                        return(<Tugas index={index + 1} subject={task.subject} title={task.title} dateCreated={task.dateCreated} />);
+                    })};
                 </section>
             </main>
         </div>
