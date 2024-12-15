5f5c0c144b91508796b4d5c377aff92e46c40ca5
Raul Maulidhino <raulahmadm314@gmail.com>
2024-12-15 13:47:07 +0700

    style: edit some stylings

--- src/components/TugasTerbaru.jsx
+++ src/components/TugasTerbaru.jsx
@@ -1,9 +1,9 @@
 const TugasTerbaru = ({index, subject, title, dateCreated}) => {
     return (
         <section className="p-2 shadow-lg rounded-xl flex gap-3">
-            <section className="text-white text-4xl bg-primary px-4 py-2 rounded-xl flex justify-center items-center">{index}</section>
+            <section className="text-white text-4xl bg-primary px-4 py-2 rounded-xl flex justify-center items-center w-14">{index}</section>
             <section>
-                <h2 className="text-lg text-blue-700 font-semibold mb-[0.5em]">{subject} | {title}</h2>
+                <h2 className="text-lg font-semibold mb-[0.5em]"><span className="text-blue-700">{subject}</span> | {title}</h2>
                 <p>{dateCreated}</p>
             </section>
         </section>
