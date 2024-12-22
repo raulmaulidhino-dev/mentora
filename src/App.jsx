6a8aeef6994f8ec0f26e95199d373075a0425d62
Raul Maulidhino <raulahmadm314@gmail.com>
2024-12-22 22:20:02 +0700

    style: modify import and route code for component renaming

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login.jsx'
 import Registrasi from './components/Registrasi.jsx'
 import Beranda from './components/Beranda.jsx'
+import MapelSaya from './components/MapelSaya.jsx'
 import TugasSaya from './components/TugasSaya.jsx'
-import TugasTerbaruSaya from './components/TugasTerbaruSaya.jsx'
 import ProfilSiswa from './components/ProfilSiswa.jsx'
 import Guru from './components/Guru.jsx'
 
@@ -14,12 +14,12 @@ const App = () => {
       <Route path="/login" exact element={<Login />} />
       <Route path="/registrasi" element={<Registrasi />} />
       <Route path="/beranda" element={<Beranda />} />
-      <Route path="/mata-pelajaran" element={<TugasSaya />} />
-      <Route path="/tugas-saya" element={<TugasTerbaruSaya />} />
+      <Route path="/mata-pelajaran" element={<MapelSaya />} />
+      <Route path="/tugas-saya" element={<TugasSaya />} />
       <Route path="/profil" element={<ProfilSiswa />} />
       <Route path="/guru" element={<Guru />} />
     </Routes>
   )
 }
 
-export default App
+export default App;
